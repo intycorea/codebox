7302eeefe4d4ff5debbf438f699dcbb6e20410c5
Inty <intycorea@users.noreply.github.com>
2018-10-29 10:43:21 -0600

    version 1.05

---
+++ index.js
@@ -0,0 +1,10 @@
+//Servidor web en nodeJS para publicar archivos estaticos.
+var express = require("express");
+var app = express();
+
+//Exponer una carpeta como publica, unicamente para archivos estaticos: .html, imagenes, .css, .js
+app.use(express.static("public"));
+
+//Crear y levantar el servidor web.
+app.listen(3004);
+console.log("Servidor inciado");
